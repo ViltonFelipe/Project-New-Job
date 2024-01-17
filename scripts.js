f46bfc60e11c744614168bfd563394bd9c622356
YuriMayk <yuri.mayk@gmail.com>
2024-01-16 23:36:18 -0300

    Developed script.js.

--- scripts.js
+++ scripts.js
@@ -1,4 +1,14 @@
-console.log("Código Conectado!")
+
+let infoCep = document.getElementsByName("cep");
+
+//  infoCep.addEventListener('onkeypress')
+
+console.log(infoCep)
+
+
+// Var - Cria uma varíavel GLOBAL que pode ser acessada de qualquer parte do código.
+// Let - Cria uma varíavel que poderá ser acessada de seu próprio escopo ou em um escopo filho do seu escopo.
+// Const - Cria uma constante em forma de variável, que poderá ser acessada de seu próprio escopo ou de um escopo que seja filho do seu próprio escopo.
 
 
 
