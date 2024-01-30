4b7f8b41cf6f26a971284e69c4fcd4eb386fb807
Vilton Siqueira <122039386+ViltonFelipe@users.noreply.github.com>
2024-01-29 21:49:34 -0300

    Limpei o código scripts.js!

--- scripts.js
+++ scripts.js
@@ -10,8 +10,3 @@ console.log(infoCep)
 // Let - Cria uma varíavel que poderá ser acessada de seu próprio escopo ou em um escopo filho do seu escopo.
 // Const - Cria uma constante em forma de variável, que poderá ser acessada de seu próprio escopo ou de um escopo que seja filho do seu próprio escopo.
 
-function confirm(){
-    alert(infoCep)
-}
-
-
